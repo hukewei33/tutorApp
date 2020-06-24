8686373a502e264db13244d171cd70c83ce47a91
Kenny Hu <hukewei33@gmail.com>
2020-06-24 10:37:19 -0400

    added gitignore and Procfile and update ports

--- app.js
+++ app.js
@@ -134,7 +134,11 @@ app.post("/students",function(req,res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
